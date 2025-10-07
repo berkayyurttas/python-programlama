cd4e10e453c9721e06bbc483bf89e001a6ba6528
Berkay Yurttaş <byurttas2004@gmail.com>
2025-10-07 13:08:43 +0300

    üçüncü commit atıldı

---
+++ der3.py
@@ -0,0 +1,10 @@
+# klavyeden girilen bir sayıanın pozitif negatif ya da sıfıra eşit olup olmadığını bulunuz.
+
+sayi = int(input("Klavyeden bir sayi giriniz: "))
+  
+if sayi > 0:
+     print("Girdiğiniz sayi pozitiftir: ")
+elif sayi < 0:
+     print("Girdiğiniz sayi negatiftir: ")  
+else:
+     print("Girilen sayi sifira eşittir")     ,
