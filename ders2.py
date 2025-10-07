b76fff2c3acfaea046a4e055b0b4c2b4fae28d87
berkayyurttas <byurttas2004@gmail.com>
2025-10-07 12:57:39 +0300

    Create ders2.py

---
+++ ders2.py
@@ -0,0 +1,10 @@
+sayi= int(input("Lütfen bir sayi giriniz: "))
+
+if sayi > 0:
+     print("Pozitiftir. ")
+
+else:
+     if sayi < 0:
+       print("Negatiftir. ")
+     else:
+        print("Sayi sifira eşittir. ")
